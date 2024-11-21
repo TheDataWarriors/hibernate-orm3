0122aa65cd6e13bdbf09ec7aa6dfecb9ee3beaa9
Čedomir Igaly <cedomir.igaly@e-dama.net>
2024-11-21 11:56:19 +0100

    HHH-18693 Updated InnerClassTest

--- tooling/metamodel-generator/src/test/java/org/hibernate/processor/test/innerclass/InnerClassTest.java
+++ tooling/metamodel-generator/src/test/java/org/hibernate/processor/test/innerclass/InnerClassTest.java
@@ -7,6 +7,7 @@ package org.hibernate.processor.test.innerclass;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.NamedQuery;
+import org.hibernate.processor.test.innerclass.InnerClassTest.One.Two;
 import org.hibernate.processor.test.util.CompilationTest;
 import org.hibernate.processor.test.util.WithClasses;
 import org.junit.Test;
@@ -14,20 +15,25 @@ import org.junit.Test;
 import static org.hibernate.processor.test.util.TestUtil.assertMetamodelClassGeneratedFor;
 import static org.hibernate.processor.test.util.TestUtil.assertNoMetamodelClassGeneratedFor;
 import static org.hibernate.processor.test.util.TestUtil.getMetaModelSourceAsString;
+import static org.junit.Assert.assertEquals;
 
 public class InnerClassTest extends CompilationTest {
 
-	@WithClasses({Person.class, Dummy.class, Inner.class})
+	@WithClasses({Person.class, Dummy.class, Inner.class, Two.class})
 	@Test
 	public void test() {
-		assertMetamodelClassGeneratedFor( Inner.class );
-		System.out.println( getMetaModelSourceAsString( Inner.class ) );
-		assertMetamodelClassGeneratedFor( Dummy.Inner.class );
-		System.out.println( getMetaModelSourceAsString( Dummy.Inner.class ) );
-		assertMetamodelClassGeneratedFor( Person.class );
+		System.out.println( getMetaModelSourceAsString( InnerClassTest.class ) );
+		System.out.println( getMetaModelSourceAsString( Dummy.class ) );
 		System.out.println( getMetaModelSourceAsString( Person.class ) );
+		assertEquals(
+				getMetaModelSourceAsString( Inner.class ),
+				getMetaModelSourceAsString( Two.class )
+		);
+		assertMetamodelClassGeneratedFor( Inner.class );
+		assertMetamodelClassGeneratedFor( Two.class );
+		assertMetamodelClassGeneratedFor( Dummy.Inner.class );
+		assertMetamodelClassGeneratedFor( Person.class );
 		assertMetamodelClassGeneratedFor( Person.PersonId.class );
-		System.out.println( getMetaModelSourceAsString( Person.PersonId.class ) );
 		assertNoMetamodelClassGeneratedFor( Dummy.class );
 		assertMetamodelClassGeneratedFor( Dummy.DummyEmbeddable.class );
 		System.out.println( getMetaModelSourceAsString( Dummy.DummyEmbeddable.class ) );
@@ -55,6 +61,15 @@ public class InnerClassTest extends CompilationTest {
 
 		public void setAddress(String address) {
 			this.address = address;
+		}
+	}
+
+	static class One {
+		@Entity
+		static class Two {
+			@Id
+			Integer id;
+			String value;
 		}
 	}
 }
